2f5b319c9a6fb44aa6ae001e40a03c5132ee2850
Ivan_Deyev <deyevivan777@mail.ru>
2026-04-21 19:49:06 +0500

    repo changes

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -2,10 +2,20 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import AsyncSessionLocal
-from app.repositories.categories import CategoryRepository
-from app.repositories.posts import PostRepository
-from app.services.categories import CategoryService
-from app.services.posts import PostService
+from app.repositories.cart import CartItemRepository
+from app.repositories.category import CategoryRepository
+from app.repositories.order import OrderRepository
+from app.repositories.product import ProductRepository
+from app.repositories.review import ReviewRepository
+from app.repositories.user import UserRepository
+
+from app.services.cart import CartService
+from app.services.category import CategoryService
+from app.services.order import OrderService
+from app.services.product import ProductService
+from app.services.review import ReviewService
+from app.services.user import UserService
+
 
 
 async def get_async_db():
@@ -14,22 +24,51 @@ async def get_async_db():
 
 
 # Зависимости для получения экземпляров репозиториев
+# Сущности у которых есть внешний ключ проверяют существование связанных
+def get_cart_repository(db: AsyncSession = Depends(get_async_db)) -> CartItemRepository:
+    return CartItemRepository(db=db)
+
+
 def get_category_repository(db: AsyncSession = Depends(get_async_db)) -> CategoryRepository:
     return CategoryRepository(db=db)
 
 
-def get_post_repository(db: AsyncSession = Depends(get_async_db)) -> PostRepository:
-    return PostRepository(db=db)
+def get_order_repository(db: AsyncSession = Depends(get_async_db)) -> OrderRepository:
+    return OrderRepository(db=db)
+
+
+def get_product_repository(db: AsyncSession = Depends(get_async_db)) -> ProductRepository:
+    return ProductRepository(db=db)
+
+
+def get_review_repository(db: AsyncSession = Depends(get_async_db)) -> ReviewRepository:
+    return ReviewRepository(db=db)
+
+
+def get_user_repository(db: AsyncSession = Depends(get_async_db)) -> UserRepository:
+    return UserRepository(db=db)
 
 
 # Зависимости для получения экземпляров сервисов
+def get_cart_service(db: AsyncSession = Depends(get_async_db)) -> CartService:
+    return CartService(cart_repo=CartItemRepository(db=db))
+
+
 def get_category_service(db: AsyncSession = Depends(get_async_db)) -> CategoryService:
     return CategoryService(category_repo=CategoryRepository(db=db))
 
 
-def get_post_service(db: AsyncSession = Depends(get_async_db)) -> PostService:
-    return PostService(
-        post_repo=PostRepository(db=db),
-        category_repo=CategoryRepository(db=db)  # Посту нужен доступ к категориям для валидации
+def get_order_service(db: AsyncSession = Depends(get_async_db)) -> OrderService:
+    return OrderService(order_repo=OrderRepository(db=db))
 
-    )
+
+def get_product_service(db: AsyncSession = Depends(get_async_db)) -> ProductService:
+    return ProductService(product_repo=ProductRepository(db=db))
+
+
+def get_review_repository(db: AsyncSession = Depends(get_async_db)) -> ReviewService:
+    return ReviewService(review_repo=ReviewRepository(db=db))
+
+
+def get_user_repository(db: AsyncSession = Depends(get_async_db)) -> UserService:
+    return UserService(user_repo=UserRepository(db=db))
